6682b17cf4920feff0488f511c9f8bf6d54e8c4b
LuizFelipe406 <luizfpereira406@gmail.com>
2023-02-16 16:15:32 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Iniciando o projeto"""
-    raise NotImplementedError
+    counter = 0
+    for student_period in permanence_period:
+        try:
+            if student_period[0] <= target_time <= student_period[1]:
+                counter += 1
+        except TypeError:
+            return None
+    return counter
