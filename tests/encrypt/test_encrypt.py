0df037e39cfa84ddd8cfc6f1ee88a4f2917d2fe5
LuizFelipe406 <luizfpereira406@gmail.com>
2023-02-16 16:22:34 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("string", 2) == "gnir_ts"
+    assert encrypt_message("string", 3) == "rts_gni"
+    assert encrypt_message("string", 10) == "gnirts"
+
+    with pytest.raises(TypeError):
+        encrypt_message(2, "string")
