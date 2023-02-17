a11e0d0e051ef3b53f9d329d541ec7363fa1b8c5
LuizFelipe406 <luizfpereira406@gmail.com>
2023-02-17 14:28:50 -0300

    Req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
-    elif low_index == high_index and word[low_index] == word[high_index]:
+    elif (
+        (low_index == high_index or low_index + 1 == high_index)
+        and word[low_index] == word[high_index]
+    ):
         return True
     elif word[low_index] == word[high_index]:
-        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
         return False
